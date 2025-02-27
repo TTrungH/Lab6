6f95e6bb4dcdc220183005b709de69663aaa07fc
Trung <hau021203a@gmail.com>
2025-02-27 13:52:26 +0700

    final

--- Factory_Method_Pattern/src/App.java
+++ Factory_Method_Pattern/src/App.java
@@ -1,24 +1,31 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
         RequestCreator lowPriorityFactory = new LowPriorityRequestCreator();
-        Request lowRequest = lowPriorityFactory.createRequest();
-        lowRequest.setPriority();
-        lowRequest.setStatus();
-        lowRequest.setExpire();
-        lowRequest.processRequest();
-
         RequestCreator mediumPriorityFactory = new MediumPriorityRequestCreator();
-        Request mediumRequest = mediumPriorityFactory.createRequest();
-        mediumRequest.setPriority();
-        mediumRequest.setStatus();
-        mediumRequest.setExpire();
-        mediumRequest.processRequest();
-
         RequestCreator highPriorityFactory = new HighPriorityRequestCreator();
-        Request highRequest = highPriorityFactory.createRequest();
-        highRequest.setPriority();
-        highRequest.setStatus();
-        highRequest.setExpire();
-        highRequest.processRequest();
+        String s = sc.next();
+        if (s.equals("low")) {
+            Request lowRequest = lowPriorityFactory.createRequest();
+            lowRequest.setPriority();
+            lowRequest.setStatus();
+            lowRequest.setExpire();
+            lowRequest.processRequest();
+        } else if (s.equals("mid")) {
+            Request mediumRequest = mediumPriorityFactory.createRequest();
+            mediumRequest.setPriority();
+            mediumRequest.setStatus();
+            mediumRequest.setExpire();
+            mediumRequest.processRequest();
+        } else {
+            Request highRequest = highPriorityFactory.createRequest();
+            highRequest.setPriority();
+            highRequest.setStatus();
+            highRequest.setExpire();
+            highRequest.processRequest();
+        }
     }
+
 }
